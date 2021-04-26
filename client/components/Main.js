d4ae53dcc0f37bb127fcf34ab1621fe645250fc7
Stone Fuglaar <fuglaarusa@gmail.com>
2021-04-26 16:25:22 -0700

    Feature: About Section

--- client/components/Main.js
+++ client/components/Main.js
@@ -17,6 +17,27 @@ class Main extends React.Component {
         <a href="https://links-lights.herokuapp.com/">Link here</a>
         <h2 />
         <h1>About</h1>
+        <p>
+          Before entering the world of tech, I was involved in the performing
+          arts. I had dabbled in coding since high school, but when I actually
+          dove into it, I absolutely fell in love. I started with self teaching,
+          and then became more and more involved until I found myself deep in an
+          immersive coding bootcamp with Fullstack Academy. There I was
+          certified in a tech stack revolving around Node.js, React, PostgreSQL,
+          and Express.js. With a huge desire to learn, I immediately became
+          fascinated by the power of technology (Especially JavaScript). This is
+          something that is changing every day and always leads to an
+          opportunity to learn and grow. I plan to combine my creative
+          background with my coding skills to effectively create, problem solve,
+          and communicate to lead to an output of success.
+        </p>
+
+        <p>
+          When I’m not coding or learning new technology, you can find me
+          playing the guitar, writing music, hiking, or finding places to
+          explore in my new home of Los Angeles.
+        </p>
+        <h1>Skills</h1>
         <h1>Connect</h1>
         <a href="github.com/stonef13">GitHub</a>
         <a href="linkedin.com/in/stonefuglaar">LinkedIn</a>
