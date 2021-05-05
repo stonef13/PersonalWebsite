43f8c51c041eb82dbbc7f87b9398ecbc85c509a1
Stone Fuglaar <fuglaarusa@gmail.com>
2021-05-04 17:25:43 -0700

    Feature: nav-bar-setup and Removal: Bootstrap

--- client/components/navbar.js
+++ client/components/navbar.js
@@ -1,23 +1,32 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 import {connect} from 'react-redux'
-import {Link} from 'react-router-dom'
+// import {Link} from 'react-router-dom'
 import {logout} from '../store'
+import {Link} from 'react-scroll'
 
 const Navbar = ({handleClick, isLoggedIn}) => (
   <div>
-    <h1>SF</h1>
     <nav>
       {isLoggedIn ? (
         <div>
           {/* The navbar will show these links after you log in */}
-          <Link to="/home">Home</Link>
+          {/* <Link to="/home">Home</Link> */}
           <a href="#" onClick={handleClick}>
             Logout
           </a>
         </div>
       ) : (
-        <div>
+        <div className="nav-container">
+          <div>
+            <h1>SF</h1>
+          </div>
+          <div className="inner-right-nav-container">
+            <h3>Work</h3>
+            <h3>About Me</h3>
+            <h3>Skills</h3>
+            <h3>Connect</h3>
+          </div>
           {/* The navbar will show these links before you log in */}
           {/* <Link to="/login">Login</Link>
           <Link to="/signup">Sign Up</Link> */}
