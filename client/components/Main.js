6ca7edb1c522f4c84336bb7aa2fd8e57692ac50c
Stone Fuglaar <fuglaarusa@gmail.com>
2021-05-21 15:40:58 -0700

    Feature: Publication photo update

--- client/components/Main.js
+++ client/components/Main.js
@@ -22,9 +22,7 @@ class Main extends React.Component {
             <h2 className="learn-more">Learn More</h2>
           </Link>
         </div>
-        {/* <div>
-          <Navbar />
-        </div> */}
+        <div>{/* <Navbar /> */}</div>
         <div className="work" id="work">
           <h1>Work</h1>
 
@@ -51,7 +49,7 @@ class Main extends React.Component {
               <div className="col-md align-self-end">
                 <a href="https://rsfuglaar.medium.com/switching-from-javascript-to-swift-2b1c4c415a16">
                   <img
-                    src="work_images/SwitchingFromJavaScriptToSwift_Work.png"
+                    src="work_images/Switching_From_JavaScript_To_Swift_Work.png"
                     alt="swift publication photo"
                     className="poster"
                   />
