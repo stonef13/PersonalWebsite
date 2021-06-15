22134652dfc3fad3a8fc4766c1776654b0a3fc06
Stone Fuglaar <fuglaarusa@gmail.com>
2021-06-15 15:56:27 -0700

    server: clean-up

--- server/api/index.js
+++ server/api/index.js
@@ -1,10 +1,6 @@
 const router = require('express').Router()
 module.exports = router
 
-router.use('/users', require('./users'))
-router.use('/projects', require('./projects'))
-router.use('/skills', require('./skills'))
-
 router.use((req, res, next) => {
   const error = new Error('Not Found')
   error.status = 404
