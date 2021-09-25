8af6e8ffb426b05384dd383cc56b770cb2dd671a
Stone Fuglaar <fuglaarusa@gmail.com>
2021-09-24 23:08:27 -0700

    Feature: refactor and description add of work section

--- client/components/Work.js
+++ client/components/Work.js
@@ -33,37 +33,119 @@ const Work = () => {
       <div className="container">
         <div className="row">
           <div className="col-md align-self-start">
-            <FadeInContainer>
-              <a href="https://fsahedgehog.herokuapp.com/">
-                <img
-                  src="work_images/HedgeHog_Work.png"
-                  alt="hedgehog poster"
-                  className="poster"
-                />
-              </a>
-            </FadeInContainer>
+            {/* <FadeInContainer> */}
+            <a href="https://fsahedgehog.herokuapp.com/">
+              <img
+                src="work_images/HedgeHog_Work.png"
+                alt="hedgehog poster"
+                className="poster"
+              />
+            </a>
+            {/* </FadeInContainer> */}
           </div>
-          <div className="col-md align-self-center">
-            <FadeInContainer>
-              <a href="https://links-lights-v2.herokuapp.com/">
+          <div className="col-md">
+            <div className="row">
+              <div className="card bg-dark">
                 <img
-                  src="work_images/LinksLights_Work.png"
-                  alt="link's lights poster"
-                  className="poster"
+                  className="card-img"
+                  src="/work_images/Hedgehog_Screenshot.png"
+                  alt="Card image"
                 />
-              </a>
-            </FadeInContainer>
+              </div>
+            </div>
+            <div className="row">
+              <p className="card-text">
+                ● Integrated recieved data into Victory.js for data
+                visualiation, which makes up over 50% of the front-end ● Worked
+                with three sperate third party APIs to request and throttle the
+                necessary data ● Created dozens of algorithms to cleanse complex
+                data
+              </p>
+              <p className="card-text">
+                <small className="text-muted">
+                  Tech Stack: React/Redux, Sequelize, Express, Victory.js
+                </small>
+              </p>
+            </div>
           </div>
-          <div className="col-md align-self-end">
-            <FadeInContainer>
-              <a href="https://rsfuglaar.medium.com/switching-from-javascript-to-swift-2b1c4c415a16">
+          {/* <div className="col-md"> */}
+          {/* </div> */}
+        </div>
+        <hr />
+        <div className="row">
+          <div className="col-md align-self-start">
+            {/* <FadeInContainer> */}
+            <a href="https://links-lights-v2.herokuapp.com/">
+              <img
+                src="work_images/LinksLights_Work.png"
+                alt="link's lights poster"
+                className="poster"
+              />
+            </a>
+            {/* </FadeInContainer> */}
+          </div>
+          <div className="col-md">
+            <div className="row">
+              <div className="card bg-dark">
                 <img
-                  src="work_images/Switching_From_JavaScript_To_Swift_Work.png"
-                  alt="swift publication photo"
-                  className="poster"
+                  className="card-img"
+                  src="/work_images/Links_Lights_Screenshot.png"
+                  alt="Card image"
                 />
-              </a>
-            </FadeInContainer>
+              </div>
+            </div>
+            <div className="row">
+              <p className="card-text">
+                ● Mastered Bootstrap in just a few hours and made main
+                contribution to the design ● Designed front and back end
+                (fullstack) for cart/checkout ● Created back-end heavy
+                algorithms to sort and organize data in order to keep the front
+                end extremely fast and reliable
+              </p>
+              <p className="card-text">
+                <small className="text-muted">
+                  Tech Stack: React/Redux, Sequelize, Express
+                </small>
+              </p>
+            </div>
+          </div>
+        </div>
+        <hr />
+        <div className="row">
+          <div className="col-md align-self-start">
+            {/* <FadeInContainer> */}
+            <a href="https://rsfuglaar.medium.com/switching-from-javascript-to-swift-2b1c4c415a16">
+              <img
+                src="work_images/Switching_From_JavaScript_To_Swift_Work.png"
+                alt="swift publication photo"
+                className="poster"
+              />
+            </a>
+            {/* </FadeInContainer> */}
+          </div>
+          <div className="col-md">
+            <div className="row">
+              <div className="card bg-dark">
+                <img
+                  className="card-img"
+                  src="/work_images/Swift_Publication_Screenshot.png"
+                  alt="Card image"
+                />
+              </div>
+            </div>
+            <div className="row">
+              <p className="card-text">
+                ● Publication on perspectives about switching from JavaScript to
+                Swift ● Covers type annotations (similar to TypeScript) ● Covers
+                everything from variable declaration to loops
+              </p>
+              <p></p>
+              <p className="card-text">
+                <small className="text-muted">
+                  Tech Focus: Swift, JavaScript
+                </small>
+              </p>
+            </div>
           </div>
         </div>
       </div>
